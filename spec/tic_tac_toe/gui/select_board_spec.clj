1c36d22edc53539d86b03ee5aa7b674a7556a3df
7vanael <kristi.n.norris777@gmail.com>
2025-05-12 10:33:02 -0700

    gui board selection complete for 3d

--- spec/tic_tac_toe/gui/select_board_spec.clj
+++ spec/tic_tac_toe/gui/select_board_spec.clj
@@ -7,15 +7,21 @@
 
 (describe "select board"
 
-  (it "sets the board-size to 3 and changes state to initialize-board if 3x3 button is pressed"
-    (let [event     {:x 144 :y 350}
+  (it "sets the board-size to 3 and changes state to board-ready if 3x3 button is pressed"
+    (let [event     {:x 288 :y 288}
           new-state (multis/mouse-clicked (test-core/state-create {:status :select-board}) event)]
       (should= (test-core/state-create {:status :board-ready :board test-board/empty-board})
                new-state)))
 
-  (it "sets the board-size to 4 and changes state to initialize-board if 4x4 button is pressed"
-    (let [event     {:x 432 :y 350}
+  (it "sets the board-size to 4 and changes state to bord-ready if 4x4 button is pressed"
+    (let [event     {:x 288 :y 432}
           new-state (multis/mouse-clicked (test-core/state-create {:status :select-board}) event)]
       (should= (test-core/state-create {:status :board-ready :board test-board/empty-4-board})
+               new-state)))
+
+  (it "sets the board-size to 3x3x3 and changes state to board-ready if 4x4 button is pressed"
+    (let [event     {:x 288 :y 576}
+          new-state (multis/mouse-clicked (test-core/state-create {:status :select-board}) event)]
+      (should= (test-core/state-create {:status :board-ready :board test-board/empty-3d-board})
                new-state)))
   )
--- src/tic_tac_toe/gui/select_board.clj
+++ src/tic_tac_toe/gui/select_board.clj
@@ -13,19 +13,23 @@
   (q/text-align :center :center)
   (q/text-size 28)
   (q/text "Choose Board Size" (/ util/screen-width 2) util/title-offset-y)
-  (util/draw-2-options-buttons type-labels))
+  (util/draw-3-buttons type-labels))
 
 (defmethod core/update-state [:gui :select-board] [state]
   state)
 
 (defmethod multis/mouse-clicked :select-board [state {:keys [x y]}]
 
-  (cond (util/button-clicked? [x y] util/opt1-of-2-rect)
+  (cond (util/button-clicked? [x y] util/opt1-of-3-rect)
         (-> state
             (assoc :board (board/new-board 3))
             (assoc :status :board-ready))
-        (util/button-clicked? [x y] util/opt2-of-2-rect)
+        (util/button-clicked? [x y] util/opt2-of-3-rect)
         (-> state
             (assoc :board (board/new-board 4))
+            (assoc :status :board-ready))
+        (util/button-clicked? [x y] util/opt3-of-3-rect)
+        (-> state
+            (assoc :board (board/new-board [3 3 3]))
             (assoc :status :board-ready))
         :else state))
