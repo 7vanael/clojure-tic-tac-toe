80a1b738f604bca53977edc755eb3067e3a458b9
7vanael <kristi.n.norris777@gmail.com>
2025-05-20 19:35:34 -0700

    May have a save working, but cannot see in test database

--- spec/tic_tac_toe/persistence/postgresql_spec.clj
+++ spec/tic_tac_toe/persistence/postgresql_spec.clj
@@ -1,7 +1,8 @@
 (ns tic-tac-toe.persistence.postgresql-spec
   (:require [speclj.core :refer :all]
             [tic-tac-toe.persistence.postgresql :as sut]
-            [next.jdbc :as jdbc]))
+            [next.jdbc :as jdbc]
+            [tic-tac-toe.core-spec :as test-core]))
 
 
 (def ttt_test_spec
@@ -11,17 +12,33 @@
 
 (def test-datasource (jdbc/get-datasource ttt_test_spec))
 
-#_(defn test-connection []
-    (jdbc/execute! datasource ["SELECT 1"]))
+(defn set-up []
+  (jdbc/execute! test-datasource ["DROP TABLE IF EXISTS state"])
+  (jdbc/execute! test-datasource ["
+  CREATE TABLE state (
+  game_id             SERIAL PRIMARY KEY,
+  board               varchar,
+  active_player_index int,
+  status              varchar,
+  x_type              varchar,
+  x_difficulty        varchar DEFAULT 'nil',
+  o_type              varchar,
+  o_difficulty        varchar DEFAULT 'nil'\n)
+  "]))
+
 
 (describe "postgresql persistence"
   (with-stubs)
-  (redefs-around [db/datasource test-datasource])
-  #_(before-all setup_test_db)
-
-  #_(after-all test-datasource ["DROP TABLE IF EXISTS state"])
+  (before-all (set-up))
 
   (it "saves a state to the database"
-      )
+      (let [result (sut/save-game (test-core/state-create {:board               test-core/first-X-4-board
+                                                           :interface :gui
+                                                           :active-player-index 1 :status :in-progress
+                                                           :x-type              :human
+                                                           :o-type              :computer
+                                                           :o-difficulty        :hard})
+                                  test-datasource)]
+        (should= 1 (:state/game_id result))))
 
   )
--- src/tic_tac_toe/persistence/postgresql.clj
+++ src/tic_tac_toe/persistence/postgresql.clj
@@ -1,16 +1,32 @@
 (ns tic-tac-toe.persistence.postgresql
   (:require [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs]))
+            [next.jdbc.result-set :as rs]
+            [next.jdbc.sql :as sql]))
 
 (def db-spec
   {:dbtype "postgresql"
    :dbname "ttt"
-   :host "localhost"})
+   :host   "localhost"})
 
-(def datasource (jdbc/get-datasource db-spec))
+(def data-source (jdbc/get-datasource db-spec))
 
-#_(defn save-game [state])
+(defn save-game [{:keys [board active-player-index status players]} & [source]]
+  (let [ds (or source data-source)
+        x-type (get-in players [0 :play-type])
+        o-type (get-in players [1 :play-type])
+        x-difficulty (or (get-in players [0 :difficulty]) "nil")
+        o-difficulty (or (get-in players [1 :difficulty]) "nil")
+        string-board (pr-str board)
+        result (sql/insert! ds :state {:board               string-board
+                                       :active_player_index active-player-index
+                                       :status              (name status)
+                                       :x_type              (name x-type)
+                                       :x_difficulty        (if (keyword? x-difficulty) (name x-difficulty) x-difficulty)
+                                       :o_type              (name o-type)
+                                       :o_difficulty        (if (keyword? o-difficulty) (name o-difficulty) x-difficulty)}
+                            {:return-keys true})]
+    result))
 
 #_(defn load-game)
 
-#_(defn delete-save[]);May not need to implement this one?
+#_(defn delete-save [])                                     ;May not need to implement this one?
