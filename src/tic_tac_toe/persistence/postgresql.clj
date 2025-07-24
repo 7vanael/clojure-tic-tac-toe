67654b38ee879bc0b6db2521c52efb51ea735dea
7vanael <kristi.n.norris777@gmail.com>
2025-07-24 15:20:35 -0700

    Updated database functionality to have load-game return the complete, unchanged state if the passed in state has a game-id, otherwise to have the same behavior as before where it will return the most recent in-progress game or a new start game if no id is in the state provided

--- src/tic_tac_toe/persistence/postgresql.clj
+++ src/tic_tac_toe/persistence/postgresql.clj
@@ -23,24 +23,24 @@
         x_type VARCHAR(50) NOT NULL,
         x_difficulty VARCHAR(50),
         o_type VARCHAR(50) NOT NULL,
-        o_difficulty VARCHAR(50),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        o_difficulty VARCHAR(50)
       )"])))
 
-(defn parse-difficulty [difficulty]
-  (if (= "nil" difficulty)
+(defn parse-possible-nil [var]
+  (if (= "nil" var)
     nil
-    (keyword difficulty)))
+    (keyword var)))
 
 (defn parse-game [sql-state]
   {:board               (edn/read-string (:board sql-state))
    :active-player-index (:active_player_index sql-state)
    :status              (keyword (:status sql-state))
-   :players             [{:character  "X" :play-type (keyword (:x_type sql-state))
-                          :difficulty (parse-difficulty (:x_difficulty sql-state))}
-                         {:character  "O" :play-type (keyword (:o_type sql-state))
-                          :difficulty (parse-difficulty (:o_difficulty sql-state))}]
-   :save                :sql})
+   :players             [{:character  "X" :play-type (parse-possible-nil (:x_type sql-state))
+                          :difficulty (parse-possible-nil (:x_difficulty sql-state))}
+                         {:character  "O" :play-type (parse-possible-nil (:o_type sql-state))
+                          :difficulty (parse-possible-nil (:o_difficulty sql-state))}]
+   :save                :sql
+   :game-id             (:game_id sql-state)})
 
 (defn update-save [ds sql-state game-id]
   (sql/update! ds :tictactoe sql-state {:game_id game-id})
@@ -50,34 +50,54 @@
   (sql/insert! ds :tictactoe sql-state
                {:return-keys true :builder-fn rs/as-unqualified-maps}))
 
+(defn keyword-or-nil [setting]
+  (if (keyword? setting) (name setting) setting))
+
 (defmethod core/save-game :sql [{:keys [board active-player-index status players game-id] :as state} & [source]]
+  (println "Save game called")
+  (prn "state:" state)
+  (prn "(:game-id state):" (:game-id state))
   (let [ds           (or source data-source)
-        x-type       (get-in players [0 :play-type])
-        o-type       (get-in players [1 :play-type])
+        x-type       (or (get-in players [0 :play-type]) "nil")
+        o-type       (or (get-in players [1 :play-type]) "nil")
         x-difficulty (or (get-in players [0 :difficulty]) "nil")
         o-difficulty (or (get-in players [1 :difficulty]) "nil")
-        string-board (pr-str board)
-        sql-state    {:board               string-board
+        string-board (or (pr-str board) "nil")
+        sql-state    {:board               (or string-board "nil")
                       :active_player_index active-player-index
-                      :status              (name status)
-                      :x_type              (name x-type)
-                      :x_difficulty        (if (keyword? x-difficulty) (name x-difficulty) x-difficulty)
-                      :o_type              (name o-type)
-                      :o_difficulty        (if (keyword? o-difficulty) (name o-difficulty) o-difficulty)}
+                      :status              (keyword-or-nil status)
+                      :x_type              (keyword-or-nil x-type)
+                      :x_difficulty        (keyword-or-nil x-difficulty)
+                      :o_type              (keyword-or-nil o-type)
+                      :o_difficulty        (keyword-or-nil o-difficulty)}
         result       (if game-id
-                       (update-save ds sql-state game-id)
-                       (first-save ds sql-state))]
+                       (do (println "updating save") (update-save ds sql-state game-id))
+                       (do (println "saving new") (first-save ds sql-state)))]
+    (prn "result:" result)
     (assoc state :game-id (:game_id result))))
 
+(defn load-existing [state ds]
+  (let [game-id (:game-id state)
+        result  (sql/query ds ["SELECT * FROM tictactoe WHERE game_id = ?" game-id]
+                           {:builder-fn rs/as-unqualified-maps})]
+    (if-not (seq result) nil (parse-game (first result)))))
+
+(defn load-last-in-progress [ds]
+  (let [result (sql/query ds ["SELECT * FROM tictactoe WHERE status = 'in-progress' ORDER BY game_id DESC LIMIT 1"]
+                          {:builder-fn rs/as-unqualified-maps})]
+    (if-not (seq result) nil (assoc (parse-game (first result)) :status :found-save))))
+
+(defn new-saved-starting-state [{:keys [interface]} ds]
+  (core/save-game (core/initial-state {:interface interface :save :sql}) ds))
+
 (defmethod core/load-game :sql [state & [source]]
-  (initialize)
-  (let [ds         (or source data-source)
-        result     (sql/query ds ["SELECT * FROM tictactoe ORDER BY game_id DESC LIMIT 1"]
-                              {:builder-fn rs/as-unqualified-maps})
-        loaded-game (delay (parse-game (first result)))]
-    (if-not (seq result)
-      (core/initial-state state)
-      (assoc @loaded-game :status :found-save :interface (:interface state)))))
+  (when-not source (initialize))
+  (let [ds (or source data-source)]
+    (if-let [loaded-game (if (:game-id state)
+                           (load-existing state ds)
+                           (load-last-in-progress ds))]
+      (assoc loaded-game :interface (:interface state))
+      (new-saved-starting-state state ds))))
 
 (defmethod core/delete-save :sql [_ & [source]]
   (let [ds        (or source data-source)
@@ -88,4 +108,4 @@
       (sql/delete! ds :tictactoe {:game_id @game-id}))))
 
 #_(defn core/get-all-games [state]
-  #_(return a map of all saved games))
+    #_(return a map of all saved games))
